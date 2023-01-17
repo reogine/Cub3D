b12ebac7ffe71c613750a72d591bf5d4dd939bd0
Mostapha Idfath <midfath@e2r9p9.1337.ma>
2023-01-17 13:58:10 +0100

    Done

--- Ft_headers/window.h
+++ Ft_headers/window.h
@@ -6,12 +6,15 @@
 /*   By: midfath <midfath@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:51:48 by midfath           #+#    #+#             */
-/*   Updated: 2023/01/17 13:44:35 by midfath          ###   ########.fr       */
+/*   Updated: 2023/01/17 13:57:57 by midfath          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINDOW_H
 # define 
 # include <stdio.h>
-#endif // !window_H
+# include <mlx.h>
+
+
+#endif // !WINDOW_h
 
