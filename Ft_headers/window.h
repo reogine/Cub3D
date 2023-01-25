1862c4b754eac618f22cc47e9b7aecedc14281ff
Mostapha Idfath <midfath@e2r10p9.1337.ma>
2023-01-25 12:58:35 +0100

    plugandplay_map

--- Ft_headers/window.h
+++ Ft_headers/window.h
@@ -6,7 +6,7 @@
 /*   By: midfath <midfath@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:51:48 by midfath           #+#    #+#             */
-/*   Updated: 2023/01/17 13:57:57 by midfath          ###   ########.fr       */
+/*   Updated: 2023/01/25 12:49:35 by midfath          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 # include <stdio.h>
 # include <mlx.h>
 
+typedef struct t_window 
+{
+    
+};
+
+void  open_window();
 
 #endif // !WINDOW_h
 
